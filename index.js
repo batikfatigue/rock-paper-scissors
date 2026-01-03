47de0741f03e8525cf1969e4bddee1fa49a00f88
batikfatigue <justinngjr@gmail.com>
2026-01-03 12:25:10 +0800

    Add getComputerChoice function
    Generates a random choice in the rock-paper-scissors game.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert('hi');
+function getComputerChoice () {
+    let numChoice = Math.random();
+    if (numChoice >= 0 && numChoice < 1/3) {
+        return 'Rock';
+    } else if (numChoice >= 1/3 && numChoice < 2/3) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice());
