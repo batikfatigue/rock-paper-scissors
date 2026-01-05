b23560c6899b3d8667867c03a33f885674ed7b68
batikfatigue <justinngjr@gmail.com>
2026-01-06 02:22:33 +0800

    Add playRound function
    Compares user input with computer choice, determines the winner using
    modular arithmetic, and updates the game scores.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+const MOVES = {
+    "rock": 0,
+    "scissors": 2,
+    "paper": 1
+};
+
 function getComputerChoice () {
     let numChoice = Math.random();
     if (numChoice >= 0 && numChoice < 1/3) {
-        return 'Rock';
+        return 'rock';
     } else if (numChoice >= 1/3 && numChoice < 2/3) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
@@ -14,10 +22,28 @@ function getHumanChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     while (!choices.includes(choice)) {
        choice = prompt('Enter choice (rock/paper/scissors):'
-            , 'rock').toLowerCase();
+            , 'scissors').toLowerCase();
     };
     console.log(choice);
+    return choice;
 }
 
-console.log(getComputerChoice());
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    let result = ((MOVES[humanChoice] - MOVES[computerChoice]) % 3 + 3) % 3;
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+    if (result === 1) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        computerScore--;
+    } else if (result === 2) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        humanScore--;
+        computerScore++;
+    } else {
+        console.log(`It's a tie! Both threw ${computerChoice}!`);
+    }
+}
+
+
+playRound(getHumanChoice(), getComputerChoice());
